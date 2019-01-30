e5009a425cb3c5241a27d7e9e9d88d3f25f269ed
xiaoyang.li <xiaoyang.li@nio.com>
2019-01-30 11:54:12 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,19 @@ import Router from "vue-router";
 
 Vue.use(Router);
 
-export default new Router({
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(";");
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i].trim();
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+const router = new Router({
     routes: [
         {
             path: "/",
@@ -26,3 +38,20 @@ export default new Router({
         }
     ]
 });
+
+router.beforeEach((to, from, next) => {
+    const cookie = document.cookie;
+    const username = getCookie("username");
+    const path = to.path;
+    if (path == "/login") {
+        next();
+        return;
+    }
+    if (!username) {
+        next({ path: "/login" });
+    } else {
+        next();
+    }
+});
+
+export default router;
