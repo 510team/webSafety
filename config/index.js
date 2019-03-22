59f5982a21334046d0c7dee45e50fc263c71a244
xiaogai.li <xiaogai.li@nio.com>
2019-03-22 14:43:33 +0800

    merge

--- config/index.js
+++ config/index.js
@@ -6,41 +6,6 @@ const serverHost = "http://localhost:8362";
 const path = require("path");
 
 module.exports = {
-<<<<<<< HEAD
-    dev: {
-        // Paths
-        assetsSubDirectory: "static",
-        assetsPublicPath: "/",
-        proxyTable: {
-            "/api": {
-                target: serverHost,
-                changeOrigin: true,
-                pathRewrite: {
-                    "^/api": ""
-                }
-            }
-        },
-
-        // Various Dev Server settings
-        host: "localhost", // can be overwritten by process.env.HOST
-        port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-        autoOpenBrowser: false,
-        errorOverlay: true,
-        notifyOnErrors: true,
-        poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
-
-        /**
-         * Source Maps
-         */
-
-        // https://webpack.js.org/configuration/devtool/#development
-        devtool: "cheap-module-eval-source-map",
-
-        // If you have problems debugging vue-files in devtools,
-        // set this to false - it *may* help
-        // https://vue-loader.vuejs.org/en/options.html#cachebusting
-        cacheBusting: true,
-=======
   dev: {
     // Paths
     assetsSubDirectory: "static",
@@ -70,7 +35,6 @@ module.exports = {
 
     // https://webpack.js.org/configuration/devtool/#development
     devtool: "cheap-module-eval-source-map",
->>>>>>> ca05175fd6613b3de05dca418937431ed2166af4
 
         cssSourceMap: true
     },
